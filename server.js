a2121f2ae34e666ed39ec6d520adf40ba5b9a887
Karim B <mattiakarim@live.it>
2019-07-15 15:21:57 +0200

    Chore: completed all MVP for the daily project

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const server = express();
 
 const db = require('./data/dbConfig.js');
 
-function getAllBudgets() {
+function getAllAccounts() {
     return db('accounts');
 }
 
-function getBudgetById(id) {
+function getAccountById(id) {
     return db('accounts').where({ id });
+}
+
+function insertNewAccount({ name, budget }) {
+    return db('accounts').insert({ name, budget });
+}
+
+function updateAccount(id, { name, budget }) {
+    return db('accounts').where({ id }).update({ name, budget });
+}
+
+function deleteAccount(id) {
+    return db('accounts').where({ id }).del();
 }
 
 
@@ -16,7 +28,7 @@ server.use(express.json());
 
 server.get('/accounts', async (req, res) => {
     try {
-        const accounts = await getAllBudgets();
+        const accounts = await getAllAccounts();
 
         res.status(200).json(accounts);
     } catch(error) {
@@ -26,44 +38,87 @@ server.get('/accounts', async (req, res) => {
     }
 });
 
-server.get('/accounts/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const account = await getBudgetById(id);
+server.get('/accounts/:id', validateId, async (req, res) => {
+    res.status(200).json(req.accountId);
+});
 
-        res.status(200).json(account);
+server.post('/accounts', async (req, res) => {
+    try {
+        const { name, budget } = req.body;
+        
+        if(req.body && Object.keys(req.body)) {
+            const newAccountId = await insertNewAccount({ name, budget });
+            const newAccount = await getAllAccounts(newAccountId);
+
+            res.status(201).json(newAccount);
+        } else {
+            res.status(404).json({
+                message: 'Name & budget are mandatory'
+            });
+        }
 
     } catch(error) {
         res.status(500).json({
-            message: 'Server error while retrieving the account'
+            message: 'Server error while creating new account'
         });
     }
 });
 
-// server.post('/budgets', async (req, res) => {
-//     try {
-//         const { }
+server.put('/accounts/:id', validateId, async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        if(req.body && Object.keys(req.body)) {
+            const { name, budget } = req.body;
+            const changedAccount = await updateAccount(id, { name, budget });
 
-//     } catch(error) {
-//         res.status(500).json({
-//             message: 'Server error while creating new budget'
-//         });
-//     }
-// });
+            res.status(200).json(changedAccount);
+        } else {
+            res.status(404).json({
+                message: 'Name & budget are mandatory'
+            });
+        }
+    } catch(error) {
+        res.status(500).json({
+            message: 'Server error while updated new account'
+        });
+    }
+});
+
+server.delete('/accounts/:id', validateId, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await deleteAccount(id);
+
+        if(deleted) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({
+                message: 'Could not find the id'
+            });
+        }
+    } catch(error) {
+        res.status(500).json({
+            message: 'Server error while deleting account'
+        });
+    }
+});
 
 
 async function validateId(req, res, next) {
     try {
         const { id } = req.params;
+        const accountId = await getAccountById(id);
 
-        if(!NaN(parseInt(id))) {
-
-            if(id) {
-                
+        if(!isNaN(parseInt(id))) {
+            if(accountId) {
+                req.accountId = accountId;
+                next();            
             } else {
-    
+                res.status(404).json({
+                    message: 'Account id not found'
+                }); 
             }
-
         } else {
             res.status(404).json({
                 message: 'The id given is not a valid number'
